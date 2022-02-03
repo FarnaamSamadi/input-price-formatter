525175b6b969496f17e8b8fccc199b5166357453
farnaam <fm.samadi70@gmail.com>
2022-02-03 18:54:49 +0330

    Add styles and app structure.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import InputPriceFormatter from '@/components/InputPriceFormatter.vue'
+
+const price = ref(0)
+</script>
 
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <InputPriceFormatter v-model="price" />
+  </div>
 </template>
 
-<style></style>
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 30px;
+  background-color: #eee;
+}
+</style>
---
+++ src/components/InputPriceFormatter.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'InputPriceFormatter',
+
+  emits: ['update:modelValue'],
+
+  props: {
+    modelValue: {
+      type: [Number, String],
+      default: ''
+    }
+  },
+
+  setup(props) {
+    return { props }
+  }
+}
+</script>
+
+<template>
+  <div class="input"></div>
+</template>
+
+<style lang="scss" scoped></style>
